da0d82f0d155404b44c929b699c4f45de636653f
Toby Driscoll <driscoll@udel.edu>
2020-05-24 20:17:01 -0400

    imported files

--- src/FundamentalsNumericalComputation.jl
+++ src/FundamentalsNumericalComputation.jl
@@ -1,5 +1,22 @@
 module FundamentalsNumericalComputation
 
-greet() = print("Hello World!")
+export FNC
+FNC = FundamentalsNumericalComputation
 
-end # module
+using LinearAlgebra, SparseArrays
+using Polynomials
+using NLsolve, DifferentialEquations
+
+include("chapter01.jl")
+include("chapter02.jl")
+include("chapter03.jl")
+include("chapter04.jl")
+include("chapter05.jl")
+include("chapter06.jl")
+include("chapter08.jl")
+include("chapter09.jl")
+include("chapter10.jl")
+include("chapter11.jl")
+include("chapter13.jl")
+
+end
