4b122bb69ea18f199c7b99151478ac028f0034d2
Toby Driscoll <driscoll@udel.edu>
2020-05-25 11:59:01 -0400

    beginning change to poly coeff order

--- src/chapter01.jl
+++ src/chapter01.jl
@@ -1,14 +1,14 @@
 """
     horner(c,x)
 
-Evaluate a polynomial whose coefficients are given in descending order in `c`,
+Evaluate a polynomial whose coefficients are given in ascending order in `c`,
 at the point `x`, using Horner's rule.
 """
 function horner(c,x)
 
 n = length(c)
-y = c[1]
-for k = 2:n
+y = c[n]
+for k in n-1:-1:1
     y = x*y + c[k]
 end
 
